099cc10dcaf539b539360c69a4dafdadbb91fdee
PRIEEE <1394368193@qq.com>
2018-10-28 23:59:28 +0800

    helloworld add commit

--- equation.c
+++ equation.c
@@ -2,3 +2,14 @@
 //ax + by = c
 //dx + ey = f
 //注意输入/输出，数据类型
+#include<stdio.h>
+
+int main()
+{
+   float a,b,c,d,e,f,x,y;
+   scanf ("%f%f%f%f%f%f",&a,&b,&c,&d,&e,&f);
+   x=(c*e-b*f)/(a*e-b*d);
+   y=(c*d-f*a)/(b*d-a*e);
+   printf ("x=%f,y=%f",x,y);
+   return 0;
+}
